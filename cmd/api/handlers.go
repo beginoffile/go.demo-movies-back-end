23236133871f6808df8e256d5a5576622594be92
Cesar <cursos.mapatupisaro@gmail.com>
2024-03-22 12:36:03 -0600

    add jwt to routes

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -182,3 +182,15 @@ func (app *application) logout(w http.ResponseWriter, r *http.Request) {
 	http.SetCookie(w, app.auth.GetExpriredRefreshCookie())
 	w.WriteHeader(http.StatusAccepted)
 }
+
+func (app *application) MovieCatalog(w http.ResponseWriter, r *http.Request) {
+
+	movies, err := app.DB.AllMovies()
+	if err != nil {
+		_ = app.errorJSON(w, err)
+		return
+	}
+
+	_ = app.writeJSON(w, http.StatusOK, movies)
+
+}
